4fdab7b0db8e714691668ec9877dd0d9a03c7bc3
temitayo65 <iloritemitayo65@gmail.com>
2022-08-18 12:18:52 +0100

    Include New Alert Controller for final version

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -14,31 +14,41 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
-    var correctAnswer = 0 
+    var correctAnswer = 0
+    var questionsAsked = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         countries += ["estonia","ireland", "italy", "poland","russia","france","germany","monaco","nigeria","spain","uk","us"]
+        setUI()
+        askQuestion(action: nil)
+    }
+    
+    
+    func setUI(){
         button1.layer.borderWidth = 1
         button2.layer.borderWidth = 1
         button3.layer.borderWidth = 1
-        
         button1.layer.borderColor = UIColor.lightGray.cgColor
         button2.layer.borderColor = UIColor.lightGray.cgColor
         button3.layer.borderColor = UIColor.lightGray.cgColor
-        
-        askQuestion(action: nil)
+    }
+    
+    
+    func setButtonUI(){
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        title = countries[correctAnswer].uppercased() + "            Score: \(score)"
     }
     
     func askQuestion(action: UIAlertAction!){
         countries.shuffle()
+        questionsAsked += 1
         correctAnswer = Int.random(in: 0...2)
-        button1.setImage(UIImage(named: countries[0]), for: .normal)
-        button2.setImage(UIImage(named: countries[1]), for: .normal)
-        button3.setImage(UIImage(named: countries[2]), for: .normal)
-        title = countries[correctAnswer].uppercased()
+        setButtonUI()
     }
     
     
@@ -52,9 +62,34 @@ class ViewController: UIViewController {
             score -= 1
         }
         
-        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "Continue", style: .default , handler: askQuestion))
-        present(ac, animated: true)
+        if questionsAsked < 10{
+            let message: String
+            if title == "Wrong"{
+                message = """
+                Your score is \(score)
+                The correct answer is flag \(correctAnswer + 1 )
+                """
+            }
+            else{
+                message = "Your score is \(score)"
+            }
+            
+            let ac = UIAlertController(title: title, message: message , preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .default , handler: askQuestion))
+            present(ac, animated: true)
+        }
+        else{
+            let ac = UIAlertController(title: title, message: "Your final score is \(score)/10", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Try again", style: .cancel){_ in
+                self.score = 0
+                self.correctAnswer = 0
+                self.questionsAsked = 0
+                self.countries.shuffle()
+                self.setButtonUI()
+            })
+            present(ac, animated: true)
+        }
+        
         
     }
     
